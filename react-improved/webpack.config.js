3c000c3a75bdf10e8c06121b6d8ac1cb5b3b251d
Kaleb Olson <kaleb.olson@gmail.com>
2017-05-10 22:04:29 -0600

    Adding a webpack based react pipeline

---
+++ react-improved/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './client/index.html',
+  filename: 'index.html',
+  inject: 'body'
+})
+
+module.exports = {
+    entry: "./client/index.js",
+    output: {
+        path: path.resolve("/dist"),
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"},
+            { test: /\.jsx$/, exclude: /node_modules/, loader: "babel-loader"}
+        ]
+    },
+
+    plugins: [HtmlWebpackPluginConfig]
+}
